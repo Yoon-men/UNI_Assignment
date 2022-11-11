d1b67683fcb30690e3c40b0ad328d05e9771f7f1
Yoonmen <sgu101310@naver.com>
2022-11-12 01:21:56 +0900

    22.11.11(FRI)

--- 확률및통계/Binomial_JoyGo.py
+++ 확률및통계/Binomial_JoyGo.py
@@ -5,19 +5,19 @@ def factorial(N) :
     if N == 0 : return 1
     elif N >= 1 : return factorial(N-1) * N
 
-def probability(k, n, p) : 
+def P(k, n, p) : 
     nCk = factorial(n) / (factorial(k)*factorial(n-k))
     return nCk * p**k * (1-p)**(n-k)
 
 if __name__ == "__main__" : 
     pLi = [1/6, 2/6, 3/6]
     nLi = [20, 30, 40, 50]
-    cLi = ["#f08080", "#66cdaa", "#4169e1"]
+    cLi = ["#f08080", "#66cdaa", "#4169e1", "#c8b2e7"]
 
-    fig, ax = plt.subplots(len(pLi),len(nLi))
+    fig, ax = plt.subplots(len(pLi))
     for i in range(len(pLi)) : 
         for j in range(len(nLi)) : 
-            data = np.array([probability(k, nLi[j], pLi[i]) for k in range(nLi[j]+1)])
-            ax[i,j].bar(list(range(nLi[j]+1)), data, color=cLi[i])
+            data = np.array([P(k, nLi[j], pLi[i]) for k in range(nLi[j]+1)])
+            ax[i].plot(list(range(nLi[j]+1)), data, color=cLi[j])
 
     plt.show()
